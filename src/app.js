3b7dc29dc582e185d91ace8f109c734ee47da62f
puttpoom <poom.bm@gmail.com>
2024-08-12 09:36:49 +0700

    Add helmet and Log

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
-
+const fs = require("fs");
+const path = require("path");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helment = require("helmet");
 //const logger = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 
 const routes = require("./routes");
 
 const app = express();
+
+app.use(helment());
 const allowedOrigin = "chrome-extension://bakgomgejopenjmjaipflkhjnnecdegm";
 
 const limiter = rateLimit({
@@ -27,6 +31,12 @@ const corsOptions = {
 app.use(limiter);
 
 app.use(morgan("dev"));
+
+const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
+app.use(morgan("combined", { stream: logStream }));
+
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 // app.use(logger);
